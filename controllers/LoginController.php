d3e88cd1a2cbabe062a5e2f27e4e3f7d401490a8
martinsakac <sakac.m@gmail.com>
2014-05-31 18:15:45 +0200

    pridany css subor, sql script, UserManager, Login Controller

---
+++ controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 31.5.2014
+ * Time: 15:53
+ */
+
+class LoginController {
+
+} 
---
+++ models/UserManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martin
+ * Date: 31.5.2014
+ * Time: 15:41
+ */
+
+class UserManager {
+
+    public function loginUser($name, $password) {
+        $query = "";
+    }
+
+} 
