26b05184b52d4842bef249b1fbce9802f978b9d6
martinsakac <sakac.m@gmail.com>
2014-07-10 21:46:16 +0200

    Naincludovany css a js subor do layout.phtml

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Martin on 8.7.2014.
+ */
+
+//$(document).ready(function() {
+//    $("#loginButton").click(function() {
+//        alert("Hahaha");
+//    });
+//});
+
+$("#loginButton").click(function(){
+   alert("Haha");
+});
+
